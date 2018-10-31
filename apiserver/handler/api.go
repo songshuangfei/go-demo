9b48c37f4620e2a0a926c8962025314b4284fd7e
Fly <1462499968@qq.com>
2018-10-31 21:30:41 +0800

    not found handler

--- apiserver/handler/api.go
+++ apiserver/handler/api.go
@@ -18,7 +18,7 @@ func Api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		handler(w, r)
 		return
 	}
-	NotFound(w, r)
+	notFoundHandler(w, r)
 }
 
 //handler函数
@@ -27,7 +27,7 @@ func carouselHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, r.URL.Query().Encode())
 }
 
-func hotAuthors(w http.ResponseWriter, r *http.Request) {
+func hotAuthorsHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hot Authors")
 	io.WriteString(w, r.URL.Query().Encode())
 }
@@ -36,5 +36,5 @@ func init() {
 	//注册handler
 	apiHandlers = make(map[string]apiHandler)
 	apiHandlers["carousel"] = carouselHandler
-	apiHandlers["hotauthors"] = hotAuthors
+	apiHandlers["hotauthors"] = hotAuthorsHandler
 }
--- apiserver/handler/notfound.go
+++ apiserver/handler/notfound.go
@@ -6,6 +6,10 @@ import (
 )
 
 func NotFound(w http.ResponseWriter, r *http.Request) {
+	notFoundHandler(w, r)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(404)
 	io.WriteString(w, r.URL.String()+" is not found")
 }
